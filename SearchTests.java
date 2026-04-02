a875f73aca1e08e4b64b887b57f5b4d7f1e3431b
user <onlyrock96@mail.ru>
2026-04-02 20:01:28 +0400

    add HW

--- SearchTests.java
+++ SearchTests.java
@@ -3,5 +3,6 @@ public class SearchTests {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println(newOMG?);
+        System.out.println("Cool");
     }
 }
