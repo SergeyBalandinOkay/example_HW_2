fdd55158cc8dcc0069c2f9425cb31b1e9cfda77e
user <onlyrock96@mail.ru>
2026-04-07 21:55:21 +0400

    Added selenide textbox test

--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -1,24 +1,14 @@
 package tests;
 
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
+
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class TextBoxTests {
+public class TextBoxTests extends TestBase {
 
-    @BeforeAll
-    static void beforeAll(){
-        Configuration.browser = "chrome";
-        Configuration.browserVersion = "144.0";
-        Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://demoqa.com";
- //       Configuration.pageLoadStrategy = "eager";
-       Configuration.timeout = 10000; // default 4000
-    }
 
     @Test
     void successfulFillFormTest() {
